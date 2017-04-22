6ae10182d0072cb98cd2599d7692b9e0040eb77a
b29308188 <b29308188@gmail.com>
2017-04-22 15:21:33 -0500

    remove unecessary files

--- network-embedding/benchmarks/src/Train.cpp
+++ network-embedding/benchmarks/src/Train.cpp
@@ -1,8 +1,31 @@
 #include "Train.hpp"
 
-Train::Train(size_t e_dimension, size_t r_dimension, double learning_rate, double margin,SamplingMethod s):
-  e_dimension_(e_dimension), r_dimension_(r_dimension), learning_rate_(learning_rate), margin_(margin), sampling_m_(s)
-{
+void Train::readData(const string& relation_file_name, const string& entity_file_name) {
+  ifstream relation_file_handler, entity_file_handler;
+  // relation reading
+  relation_file_handler.open(relation_file_name, std::ifstream::in);
+  readWeights(relation_file_handler, relation_mat_, r_dimension_);
+  relation_file_handler.close();
+  // entity reading
+  entity_file_handler.open(entity_file_name, std::ifstream::in);
+  readWeights(entity_file_handler, entity_mat_, e_dimension_);
+  entity_file_handler.close();
+}
+
+void Train::readWeights(ifstream& fileHandler, std::vector<features_t>& dataMat, const size_t& dimension) {
+    std::string eachLine;
+    // count the total number of line
+    // recall that countLine will recover state for you, dont worry
+    auto numLines = countLine(fileHandler);
+    dataMat.reserve(numLines);
+    while (std::getline(fileHandler, eachLine)) {
+        std::istringstream inputStream(eachLine);
+        features_t newFeatureVec(dimension, 0.0);
+        for (size_t i = 0 ; i < dimension ; ++i ) {
+            inputStream >> newFeatureVec[i];
+        }
+        dataMat.push_back(newFeatureVec);
+    }
 }
 
 void Train::run() {
@@ -10,7 +33,6 @@ void Train::run() {
   entity_sampler_ = uniform_int_distribution<>(0, entity_mat_.size());
   triplet_sampler_ = uniform_int_distribution<>(0, triplets_.size());
 
-  const int batchsize = triplets_.size();
   loss_= 0.0;
 
   // for each epoch
@@ -19,7 +41,7 @@ void Train::run() {
     loss_= 0.0;
     // for each batch
     for ( size_t batch_idx = 0; batch_idx < num_batches_; batch_idx ++) {
-      // keep the current value of realtion vectors and entity vectors
+      // keep the current value of relation std::vectors and entity vectors
       relation_tmp_ = relation_mat_;
       entity_tmp_ = entity_mat_;
       batchUpdate();
@@ -35,18 +57,22 @@ void Train::run() {
 
 void Train::batchUpdate(){
 
+  auto batchsize = triplets_.size()/num_batches_;
+
   for ( size_t idx; idx < batchsize; idx ++ ) {
     // sample a triplet
-    auto triplet_idx = triplet_sampler(gen);
+    auto triplet_idx = triplet_sampler_(Utils::generator);
     // get the head and the tail
-    auto head_id = std::get<0> triplets_[triplet_idx];
-    auto tail_id = std::get<1> triplets_[triplet_idx];
-    auto relation_id = std::get<2> tripletes_[triplet_idx];
+    size_t head_id, tail_id, relation_id;
+    std::tie(head_id, tail_id, relation_id) = triplets_[triplet_idx];
 
-    // throw a coin to decide to remove head_id or remove tail_id
+      // throw a coin to decide to remove head_id or remove tail_id
     auto coin_num = Utils::coin(Utils::generator);
-    const auto& exist_entities = coin_num? relation_id_table[ make_pair(head_id, relation_id)]:inverse_relation_id_table[make_pair(tail_id, relation_id)]
-    size_t replace_id = negative_sample(exist_entities, entity_sampler);
+
+    const auto& exist_entities = 
+      coin_num? relation_id_table[head_id][relation_id]:inverse_relation_id_table[tail_id][relation_id];
+
+    size_t replace_id = negative_sample(exist_entities, entity_sampler_);
     // replace tail
     if (coin_num == 0) 
       weightUpdate(head_id, tail_id, relation_id, head_id, replace_id, relation_id);
@@ -72,40 +98,50 @@ void Train::weightUpdate(size_t head_id, size_t tail_id, size_t relation_id,
   auto& comp_tail = entity_mat_[comp_tail_id];
   auto& comp_relation = relation_mat_[comp_head_id];
 
-  auto sum1 = computeDist( 2, tail_vec, head_vec + relation_vec);
-  auto sum2 = computeDist( 2, comp_tail, comp_head + compe_relation);
+  auto sum1 = computeDist( norm_flag_, tail_vec, head_vec + relation_vec);
+  auto sum2 = computeDist( norm_flag_, comp_tail, comp_head + comp_relation);
+
+  // if too far, compute gradient and do update 
   if ( sum1 + margin_ > sum2) {
     loss_+= margin_ + sum1 - sum2;
-    computeGradient( head_id, tail_id, relation_id, comp_head_id, comp_tail_id, comp_relation_id);
+    updateGradient( head_vec, tail_vec, relation_vec);
+    updateGradient( comp_head, comp_tail, comp_relation);
   }
 }
 
-void  Train::computeGraident(size_t head_id, size_t tail_id, size_t relation_id, 
-                      size_t comp_head_id, size_t comp_tail_id, size_t comp_relation_id) {
-  auto& head_vec = entity_mat_[head_id];
-  auto& tail_vec = entity_mat_[tail_id];
-  auto& relation_vec = relation_mat_[relation_id];
+void Train::updateGradient(features_t& head_vec, features_t& tail_vec, features_t& relation_vec) {
+	// First, compute difference
+	auto gradVec = tail_vec - ( head_vec + relation_vec);
+	// Transform from difference to gradient
+	std::transform(gradVec.begin(), gradVec.end(), gradVec.begin(), 
+			[this] (double& val) {
+				if(norm_flag_ == 1)
+					return (val>0) ? 1.0:-1.0;
+				else
+					return 2*val;
+			}
+			);	
 
-  auto& comp_head = entity_mat_[comp_head_id];
-  auto& comp_tail = entity_mat_[comp_tail_id];
-  auto& comp_relation = relation_mat_[comp_head_id];
+	// Do update on each std::vector
+	// For each element, minus gradient
+	auto minusFunc = [this](double& grad, double& elem) {
+		auto update = -1*grad*learning_rate_;
+		return elem - update;
+	};
+	// For each element, plus graident
+	auto plusFunc = [this](double& grad, double& elem) {
+		auto update = -1*grad*learning_rate_;
+		return elem + update;
+	};
 
-  for ( int idx = 0 ; idx < e_dimension_ ; idx++ ) {
-    double distGrad = getDistanceGrad(norm_flag_, tail_vec[idx], head_vec[idx] + relation_vec[idx]);
-    double diff = distGrad*-1*learning_rate_;
+	std::transform(gradVec.begin(), gradVec.end(), 
+				relation_vec.begin(), relation_vec.begin(), minusFunc);
 
-    relation_tmp_[relation_id][idx] -= diff;
-    entity_tmp_[head_id][idx] -= diff;
-    entity_tmp_[tail_id][idx] += diff;
+	std::transform(gradVec.begin(), gradVec.end(), 
+				head_vec.begin(), head_vec.begin(), minusFunc);
 
-    double comp_distGrad = getDistanceGrad(norm_flag_, comp_tail[idx], comp_head[idx] + comp_relation[idx]);
-
-    double comp_diff = comp_distGrad*-1*learning_rate_;
-
-    relation_tmp_[comp_relation_id][idx] -= comp_diff;
-    entity_tmp_[comp_head_id][idx] -= comp_diff;
-    entity_tmp_[comp_tail_id][idx] += comp_diff;
-  }
+	std::transform(gradVec.begin(), gradVec.end(),
+			tail_vec.begin(), tail_vec.begin(), plusFunc);
 }
 
 
@@ -114,13 +150,13 @@ void Train::writeData() {
   ofstream relation_file, entity_file;
   relation_file.open(relation_file_name, ios::out|ios::trunc);
   entity_file.open(relation_file_name, ios::out|ios::trunc);
-  // print out each realtion vector
+  // print out each relation std::vector
   for( auto relation_vec : relation_mat_) {
     for ( auto element : relation_vec)
       relation_file << setprecision(7) << element << '\t' ;
     relation_file << endl;
   }
-  // print out each entity vector
+  // print out each entity std::vector
   for ( auto entity_vec : entity_mat_ ) {
     for ( auto element : entity_vec) 
       entity_file << setprecision(7) << element << '\t';
--- network-embedding/benchmarks/src/Train.hpp
+++ network-embedding/benchmarks/src/Train.hpp
@@ -12,14 +12,16 @@ enum class SamplingMethod {
 
 // (head, tail, relationship)
 using triplet_t = tuple<size_t, size_t, size_t>;
+using features_t = std::vector<double>;
 
 class Train {
 public:
-  Train(size_t e_dimension, size_t r_dimension, double learning_rate, double margin,SamplingMethod s);
+	Train(size_t e_dimension, size_t r_dimension, 
+			double learning_rate, double margin,SamplingMethod s): e_dimension_(e_dimension), r_dimension_(r_dimension), learning_rate_(learning_rate), margin_(margin), sampling_m_(s) {}
   /*
    * Read pre-processed data
    */
-  void readData();
+  void readData(const string& relation_file_name, const string& entity_file_name);
 
   /*
    * Traverse all relationship and update   
@@ -34,12 +36,14 @@ public:
  * private functions
  */
 private:
+  // read weight vectors from a given file
+  void readWeights(ifstream& ifs, std::vector<features_t>& dataMat, const size_t& dimension);
   // update a mini batch
   void batchUpdate();
   // update weight
-  void weightUpdate();
+  void weightUpdate(size_t heada, size_t taila, size_t rela, size_t headb, size_t tailb, size_t relb);
 
-  void computeGraident();
+  void updateGradient(features_t& head_vec, features_t& tail_vec, features_t& relation_vec);
 
 /*
  * Data
@@ -56,18 +60,24 @@ private:
   SamplingMethod sampling_m_ = SamplingMethod::UniformDistribution;
   
   vector<triplet_t> triplets_; 
-  vector<vector<int> > feature_;
-  vector<vector<double> > relation_mat_, entity_mat_;
-  vector<vector<double> > relation_tmp_, entity_tmp_;
+  vector<vector<int>> feature_;
+  vector<features_t> relation_mat_, entity_mat_;
+  vector<features_t> relation_tmp_, entity_tmp_;
 
   string relation_file_name = "relation_mat";  
   string entity_file_name = "entity_vec";
 
-  uniform_int_distribution<> entity_sampler_;
-  uniform_int_distribution<> triplet_sampler_;
+  uniform_int_distribution<int> entity_sampler_;
+  uniform_int_distribution<int> triplet_sampler_;
 
   double loss_ = 0.0;
 
   int norm_flag_ = 2;
+
+  using uset_t = unordered_set<size_t>;
+  using table_t = unordered_map<size_t, uset_t>;
+
+  unordered_map<size_t, table_t> relation_id_table;
+  unordered_map<size_t, table_t> inverse_relation_id_table;
 };
 
